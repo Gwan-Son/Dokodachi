3ced4cef311bfe2a87b6c91e231bf4e59be39563
GwanSon <id147369@naver.com>
2024-06-28 17:06:56 +0900

    update 24.06.28

--- Dokodachi/ViewController.swift
+++ Dokodachi/ViewController.swift
@@ -6,12 +6,60 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class ViewController: UIViewController {
 
+    var idField: UITextField!
+    var pwField: UITextField!
+    var loginButton: UIButton!
+    var idValidView: UIView!
+    var pwValidView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // idField
+        idField = UITextField()
+        idField.placeholder = "id를 입력해주세요."
+        idField.translatesAutoresizingMaskIntoConstraints = false
+        idField.font = UIFont.systemFont(ofSize: 18)
+        idField.textColor = .black
+        view.addSubview(idField)
+        
+        // pwField
+        pwField = UITextField()
+        pwField.placeholder = "pw를 입력해주세요."
+        pwField.translatesAutoresizingMaskIntoConstraints = false
+        pwField.font = UIFont.systemFont(ofSize: 18)
+        pwField.textColor = .black
+        view.addSubview(pwField)
+        
+        // loginButton
+        loginButton = UIButton()
+        loginButton.setTitle("로그인", for: .normal)
+        loginButton.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(loginButton)
+        
+        // idValidView
+        idValidView = UIView()
+        idValidView.backgroundColor = .red
+        idValidView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(idValidView)
+        
+        // pwValidView
+        pwValidView = UIView()
+        pwValidView.backgroundColor = .red
+        pwValidView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(pwValidView)
+        
+        NSLayoutConstraint.activate([
+            idField.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
+            idField.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            pwField.topAnchor.constraint(equalTo: idField.topAnchor, constant: 20),
+            pwField.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
     }
 
 
