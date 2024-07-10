da72b6324b11f10baf5d4713cff856b3f209ad2e
GwanSon <id147369@naver.com>
2024-07-10 18:26:38 +0900

    update 24.07.10

--- Dokodachi/ChatMessageCell.swift
+++ Dokodachi/ChatMessageCell.swift
@@ -9,53 +9,50 @@ import UIKit
 
 class ChatMessageCell: UITableViewCell {
     let messageLabel = UILabel()
-    let bubbleBackgroundView = UIView()
-    
-    var leadingConstraint: NSLayoutConstraint!
-    var trailingConstraint: NSLayoutConstraint!
-    
-    var isIncoming: Bool = true {
+    var isIncoming: Bool = false {
         didSet {
-            bubbleBackgroundView.backgroundColor = isIncoming ? .white : .darkGray
-            messageLabel.textColor = isIncoming ? .black : .white
-            
-            leadingConstraint.isActive = isIncoming
-            trailingConstraint.isActive = !isIncoming
+            updateUI()
         }
     }
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         
-        backgroundColor = .clear
-        
-        bubbleBackgroundView.backgroundColor = .yellow
-        bubbleBackgroundView.layer.cornerRadius = 12
-        bubbleBackgroundView.translatesAutoresizingMaskIntoConstraints = false
-        addSubview(bubbleBackgroundView)
-        
-        messageLabel.numberOfLines = 0
-        messageLabel.translatesAutoresizingMaskIntoConstraints = false
-        addSubview(messageLabel)
-        
-        let constraints = [
-            messageLabel.topAnchor.constraint(equalTo: topAnchor, constant: 16),
-            messageLabel.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -16),
-            messageLabel.widthAnchor.constraint(lessThanOrEqualToConstant: 250),
-            
-            bubbleBackgroundView.topAnchor.constraint(equalTo: messageLabel.topAnchor, constant: -8),
-            bubbleBackgroundView.leadingAnchor.constraint(equalTo: messageLabel.leadingAnchor, constant: -8),
-            bubbleBackgroundView.bottomAnchor.constraint(equalTo: messageLabel.bottomAnchor, constant: 8),
-            bubbleBackgroundView.trailingAnchor.constraint(equalTo: messageLabel.trailingAnchor, constant: 8),
-        ]
-        
-        NSLayoutConstraint.activate(constraints)
-        
-        leadingConstraint = messageLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 32)
-        trailingConstraint = messageLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -32)
+        setupUI()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    private func setupUI() {
+        messageLabel.numberOfLines = 0
+        messageLabel.translatesAutoresizingMaskIntoConstraints = false
+        contentView.addSubview(messageLabel)
+        
+        NSLayoutConstraint.activate([
+            messageLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 10),
+            messageLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -10),
+            messageLabel.widthAnchor.constraint(lessThanOrEqualTo: contentView.widthAnchor, multiplier: 0.75)
+        ])
+        
+        updateUI()
+    }
+    
+    private func updateUI() {
+        if isIncoming {
+            messageLabel.textAlignment = .left
+            messageLabel.textColor = .black
+            messageLabel.backgroundColor = UIColor(white: 0.9, alpha: 1)
+            messageLabel.layer.cornerRadius = 8
+            messageLabel.layer.masksToBounds = true
+        } else {
+            messageLabel.textAlignment = .right
+            messageLabel.textColor = .white
+            messageLabel.backgroundColor = UIColor.blue
+            messageLabel.layer.cornerRadius = 8
+            messageLabel.layer.masksToBounds = true
+        }
+    }
 }
+
